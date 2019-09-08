16bffc03adebc2b69ae7f2c4936a070bb7478087
manthan jain <mnthnjain@gmail.com>
2019-09-08 07:29:50 +0530

    adding angular code as second library

--- server.js
+++ server.js
@@ -95,10 +95,10 @@ if (env === "dev") {
   app.all('/*', (req, res) => { apiProxy.web(req, res, { target: 'http://localhost:4200', }); }) 
 } else {
   // Run the app by serving the static files in the dist directory
-  app.use(express.static(__dirname + '/appointment-client'));
+  app.use(express.static(__dirname + '/dist'));
   // For all GET requests, send back index.html so that PathLocationStrategy can be used
   app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/appointment-client/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
   });
 }
 app.listen(envSetup[env].port || 3000);
